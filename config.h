593b6f24eea34383c2b943eaac72603555065054
Artur Dobrowolski <artur.dobrowolski@startmail.com>
2023-02-03 14:38:39 +0100

    do not launch fs-uae in child window

--- config.h
+++ config.h
@@ -43,7 +43,7 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "FS-UAE",       0,         0,          0,           1,        -1 }, /* fs-uae */
+	/*{ NULL,      NULL,     "FS-UAE",       0,         0,          0,           1,        -1 }, fs-uae */
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
