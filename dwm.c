94fb3b0f85ceb085430f6ccaae7b1b22a14c9a2c
artur.dobrowolski@startmail.com <artur.dobrowolski@startmail.com>
2024-07-22 20:43:14 +0200

    More keybinds and code cleanup

--- dwm.c
+++ dwm.c
@@ -49,7 +49,7 @@
 #define BUTTONMASK              (ButtonPressMask|ButtonReleaseMask)
 #define CLEANMASK(mask)         (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
 #define INTERSECT(x,y,w,h,m)    (MAX(0, MIN((x)+(w),(m)->wx+(m)->ww) - MAX((x),(m)->wx)) \
-                               * MAX(0, MIN((y)+(h),(m)->wy+(m)->wh) - MAX((y),(m)->wy)))
+                                    * MAX(0, MIN((y)+(h),(m)->wy+(m)->wh) - MAX((y),(m)->wy)))
 #define ISVISIBLE(C)            ((C->tags & C->mon->tagset[C->mon->seltags]) || C->issticky)
 #define LENGTH(X)               (sizeof X / sizeof X[0])
 #define MOUSEMASK               (BUTTONMASK|PointerMotionMask)
@@ -595,8 +595,8 @@ cleanup(void)
 		cleanupmon(mons);
 	for (i = 0; i < CurLast; i++)
 		drw_cur_free(drw, cursor[i]);
- 	for (i = 0; i < LENGTH(colors); i++)
- 		free(scheme[i]);
+	for (i = 0; i < LENGTH(colors); i++)
+		free(scheme[i]);
 	for (i = 0; i < LENGTH(tagsel); i++)
 		free(tagscheme[i]);
 	free(scheme);
